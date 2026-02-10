4060fc3e90bda84625816cee8fed62e8d92a4bfa
unknown <budzynkarol18@gmail.com>
2026-02-10 12:19:44 +0100

    initial commit

---
+++ oferty/management/commands/ensure_superuser.py
@@ -0,0 +1,24 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Tworzy superusera z env vars jesli nie istnieje"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "")
+
+        if not username or not password:
+            self.stdout.write("DJANGO_SUPERUSER_USERNAME / DJANGO_SUPERUSER_PASSWORD not set, skipping.")
+            return
+
+        if User.objects.filter(username=username).exists():
+            self.stdout.write(f"Superuser '{username}' already exists.")
+            return
+
+        User.objects.create_superuser(username=username, email=email, password=password)
+        self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created."))
